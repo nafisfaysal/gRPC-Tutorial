619b4282c65ae3130096f4eb8bc77bf37093bf09
Nafis Faysal <7595238+nafisfaysal@users.noreply.github.com>
2018-06-12 00:47:28 +0600

    fix wrong importer and fixed bug for python client

--- Client/Python/Calc_Client.py
+++ Client/Python/Calc_Client.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 import grpc
 
 import Calc_pb2
+import Calc_pb2_grpc
 
 def run():
   channel = grpc.insecure_channel('localhost:50050')
-  stub = Calc_pb2.CalculatorStub(channel)
+  stub = Calc_pb2_grpc.CalculatorStub(channel)
   response = stub.Add(Calc_pb2.AddRequest(n1=20,n2=10))
   print(response.n1)
   response = stub.Substract(Calc_pb2.SubstractRequest(n1=20,n2=10))
