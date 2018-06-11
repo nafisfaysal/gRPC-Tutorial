d0e93cff331c40dea2a65ba48958379d9c042e75
Nafis Faysal <7595238+nafisfaysal@users.noreply.github.com>
2018-06-12 00:49:39 +0600

    fix bug for python server

--- Server/Calc_Server.py
+++ Server/Calc_Server.py
@@ -4,11 +4,12 @@ import time
 import grpc
 
 import Calc_pb2
+import Calc_pb2_grpc
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
-class Calculator(Calc_pb2.CalculatorServicer):
+class Calculator(Calc_pb2_grpc.CalculatorServicer):
 
   def Add(self, request, context):
     return Calc_pb2.AddReply(n1=request.n1+request.n2)
@@ -25,7 +26,7 @@ class Calculator(Calc_pb2.CalculatorServicer):
 
 def serve():
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  Calc_pb2.add_CalculatorServicer_to_server(Calculator(), server)
+  Calc_pb2_grpc.add_CalculatorServicer_to_server(Calculator(), server)
   server.add_insecure_port('[::]:50050')
   server.start()
   try:
